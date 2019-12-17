0af8f797060037636fc671560487b247e8e92796
Scott Payton <spayton.c@gmail.com>
2019-12-17 11:48:56 -0700

    Need to fix breaker button to read element data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+const fetch = require("node-fetch")
 import $ from 'jquery'
 
 $(document).ready(() => {
   // have fun!
-})
+  $.get("https://wordwatch-api.herokuapp.com/api/v1/top_word", function(data){
+    $('.top-word').append(Object.keys(data.word))
+    $('.word-count').append(Object.values(data.word))
+}); //end get
+
+  $('#breaker').click(function() {
+    var textStack = $('textArea').val();
+    console.log(textStack)
+  });
+
+
+}); //end doc .ready
